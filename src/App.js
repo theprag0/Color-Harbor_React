e28b3592579d4b4a374cc36eed1b8a0a66bfc985
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-05-17 17:42:47 +0530

    add copy overlay animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Palette {...seedColors[4]}/>
       </div>
     );
