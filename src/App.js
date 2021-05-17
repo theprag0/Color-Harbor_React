af0ffff4b22f37d7362ae8ed22e18ded5d464b3d
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-05-17 21:57:52 +0530

    add color helpers to generate colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {generatePalette} from './colorHelpers';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[0]))
     return (
       <div className="App">
         <Palette {...seedColors[4]}/>
---
+++ src/colorHelpers.js
@@ -0,0 +1,47 @@
+import chroma from 'chroma-js';
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+const generatePalette = starterPalette => {
+    let newPalette = {
+        paletteName: starterPalette.paletteName,
+        id: starterPalette.id,
+        emoji: starterPalette.emoji,
+        colors: {}
+    };
+
+    for (let level of levels) {
+        newPalette.colors[level] = [];
+    }
+    for (let color of starterPalette.colors) {
+        let scale = getScale(color.color, 10);
+        for (let i in scale) {
+            newPalette.colors[levels[i]].push({
+                name: `${color.name} ${levels[i]}`,
+                id: color.name.toLowerCase().replace("/ /g", "-"),
+                hex: scale[i],
+                rgb: chroma(scale[i]).css(),
+                rgba: chroma(scale[i]).css().replace("rgb", "rgba").replace(")", ",0.1)")
+            })
+        }
+    }
+
+    return newPalette;
+};
+
+const getRange = hexColor => {
+    let end = "#fff";
+    return [
+        chroma(hexColor).darken(1.4).hex(),
+        hexColor,
+        end
+    ];
+};
+
+const getScale = (hexColor, numOfColors) => {
+    return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numOfColors);
+};
+
+export {generatePalette};
