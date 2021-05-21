2842380f37f6b85d3b77434454f5e96acbc414c4
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-05-21 22:09:19 +0530

    tweak styles of form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import Button from '@material-ui/core/Button';
 import styles from './styles/PaletteFormNavStyles';
 
@@ -57,7 +57,7 @@ class PaletteFormNav extends Component{
                         onClick={this.props.handleDrawerOpen}
                         className={classNames(classes.menuButton, open && classes.hide)}
                     >
-                        <MenuIcon />
+                        <AddToPhotosIcon fontSize="small" />
                     </IconButton>
                     <Typography variant="h6" color="inherit" noWrap>
                         Create A Palette
--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -17,6 +17,7 @@ const styles = theme => ({
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
+    width: '100%',
     padding: '0 8px',
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
